eff433309b943f57f37d41a2f76eb917542b28b5
Jacob Schandel <jacob@schandelvalley.com>
2018-05-28 11:49:37 -0400

    c

--- Python/LightClass.py
+++ Python/LightClass.py
@@ -6,47 +6,46 @@ boatlighting | LightClass.py
 from gpiozero import DigitalOutputDevice
 
 class ExternalLighting:
-    def __init__ (self):
-        self.red = DigitalOutputDevice(17)
-        self.green = DigitalOutputDevice(27)
-        self.blue = DigitalOutputDevice(22)
+    red = DigitalOutputDevice(17)
+    green = DigitalOutputDevice(27)
+    blue = DigitalOutputDevice(22)
 
     def off(self):
-        self.red.off()
-        self.green.off()
-        self.blue.off()
+        red.off()
+        green.off()
+        blue.off()
 
     def blue(self):
-        self.red.off()
-        self.green.off()
-        self.blue.on()
+        red.off()
+        green.off()
+        blue.on()
 
     def green(self):
-        self.red.off()
-        self.green.on()
-        self.blue.off()
+        red.off()
+        green.on()
+        blue.off()
 
     def teal(self):
-        self.red.off()
-        self.green.on()
-        self.blue.on()
+        red.off()
+        green.on()
+        blue.on()
 
     def red(self):
-        self.red.on()
-        self.green.off()
-        self.blue.off()
+        red.on()
+        green.off()
+        blue.off()
 
     def purple(self):
-        self.red.on()
-        self.green.off()
-        self.blue.on()
+        red.on()
+        green.off()
+        blue.on()
 
     def amber(self):
-        self.red.on()
-        self.green.on()
-        self.blue.off()
+        red.on()
+        green.on()
+        blue.off()
 
     def white(self):
-        self.red.on()
-        self.green.on()
-        self.blue.on()
+        red.on()
+        green.on()
+        blue.on()
