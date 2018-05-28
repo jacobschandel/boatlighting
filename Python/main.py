9a2c63539aebde55aff61d450ded26e904b184ef
Jacob Schandel <jacob@schandelvalley.com>
2018-05-28 12:58:52 -0400

    added shutdown, streamlined relay operations

--- Python/main.py
+++ Python/main.py
@@ -7,6 +7,7 @@ from gpiozero import DigitalOutputDevice
 from gpiozero import RGBLED
 from gpiozero import Button
 from time import sleep
+import subprocess
 
 # light  controls
 selectionLED = RGBLED(2,3,4)
@@ -22,57 +23,58 @@ apply = Button(21)
 
 print("PROGRAM ACTIVE")
 
-#TODO: add a second buton, a while loop, and an ability to
-#      perform a proper shutdown
+red.off()
+green.off()
+blue.off()
 
 selectionLED.color = (0,0,0)
-while 1==1: #TODO: add in support for lightRelays1
+while kill.is_pressed == False: #TODO: add in support for lightRelays1
     selectionLED.pulse(1,1,(0,0,1)) #blue
-    red.off()
-    green.off()
     blue.on()
     apply.wait_for_press()
     selectionLED.off()
+    blue.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1,1,(0,1,0)) #green
-    red.off()
     green.on()
-    blue.off()
     apply.wait_for_press()
     selectionLED.off()
+    green.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1,1,(0,1,1)) #teal
-    red.off()
     green.on()
     blue.on()
     apply.wait_for_press()
     selectionLED.off()
+    green.off()
+    blue.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1,1,(1,0,0)) #red
     red.on()
-    green.off()
-    blue.off()
     apply.wait_for_press()
     selectionLED.off()
+    red.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1,1,(1,0,1)) #magenta
     red.on()
-    green.off()
     blue.on()
     apply.wait_for_press()
     selectionLED.off()
+    red.off()
+    blue.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1,1,(1,1,0)) #yellowish-orange
     red.on()
     green.on()
-    blue.off()
     apply.wait_for_press()
     selectionLED.off()
+    red.off()
+    green.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1,1,(1,1,1)) #white
@@ -81,49 +83,43 @@ while 1==1: #TODO: add in support for lightRelays1
     blue.on()
     apply.wait_for_press()
     selectionLED.off()
+    red.off()
+    blue.off()
+    green.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1,1,(0,0,1), (0,1,0)) #ice
     while apply.is_pressed == False:
-        red.off()
         green.off()
         blue.on()
         sleep(1)
-        red.off()
         green.on()
-        blue.on()
         sleep(1)
-        red.off()
-        green.on()
         blue.off()
         sleep(1)
     selectionLED.off()
+    green.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1,1,(1,0,0),(1,1,0)) #fire
     while apply.is_pressed == False:
         red.on()
-        green.off()
         blue.off()
         sleep(1)
-        red.on()
         green.on()
-        blue.off()
         sleep(1)
-        red.on()
         green.off()
         blue.on()
         sleep(1)
     selectionLED.off()
+    red.off()
+    blue.off()
     apply.wait_for_release()
 
     selectionLED.pulse(1.25,1.25,(1,0,0),(0,0,1))#American flag
     while apply.is_pressed == False:
         red.on()
-        green.off()
-        blue.off()
         sleep(1)
-        red.on()
         green.on()
         blue.on()
         sleep(1)
@@ -131,33 +127,32 @@ while 1==1: #TODO: add in support for lightRelays1
         green.off()
         sleep(1)
     selectionLED.off()
+    blue.off()
     apply.wait_for_release()
 
     selectionLED.pulse(.25,.25,(1,0,1), (0,1,1)) #WILD!!!
     while apply.is_pressed == False:
         red.on() #purple
-        green.off()
         blue.on()
         sleep(0.5)
-        red.on() #red
-        green.off()
-        blue.off()
+        blue.off() #red
         sleep(0.5)
-        red.on()
         green.on() #amber
-        blue.off()
         sleep(0.5)
         red.off() #green
-        green.on()
-        blue.off()
         sleep(0.5)
-        red.off() #teal
-        green.on()
-        blue.on()
+        blue.on() #teal
         sleep(0.5)
-        red.off() #blue
-        green.off()
-        blue.on()
+        green.off() #blue
         sleep(0.5)
     selectionLED.off()
+    blue.off()
     apply.wait_for_release()
+red.off()
+green.off()
+blue.off()
+
+command = "/usr/bin/sudo /sbin/shutdown now"
+process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+output = process.communicate()[0]
+print(output)
