987f871cae68a3833fee092de08ce332cb1582d5
Jacob Schandel <jacob@schandelvalley.com>
2019-06-12 13:28:49 -0400

    added code for functions and commentary functions need filled yet

--- rewrite/main.cpp
+++ rewrite/main.cpp
@@ -6,11 +6,15 @@
 
 #include <iostream>
 #include <wiringPi.h>
+#include "ws2812-rpi.h"
 
-// Note: wiringPi.h is part of Raspbian, and may show as an error when writing code on a device not
-//       Raspbian-based, such as a Windows PC code editor with debugging.
+// Note: wiringPi.h is part of Raspbian, and may show as an error when writing code on a device
+//       not Raspbian-based, such as a Windows PC code editor with debugging.
 
 using namespace std;  // TODO: replace with proper usings.
+
+// GPIO CONSTANTS
+// ==============
 
 // The following is the GPIO assignments for the project, defined as const ints:
 //     Board Indicator RGB (optional): 2, 3, 4, for red, green, and blue, respectively
@@ -19,25 +23,218 @@ using namespace std;  // TODO: replace with proper usings.
 //     Blue Relay for Exterior: 22
 //     Color Toggle: 21
 //     Reset System Button: 20
-//     Signal Pin for Interior: TBD
+//     Signal Pin for Interior: 18
 
 const int RGB_LED[3] = {2, 3, 4};
 const int RGB_EXT[3] = {17, 27, 22};
-const int RGB_INT_ADDR = -1;  // TODO: change to the chosen pin
+const int RGB_INT_ADDR = 18;  // this is defined in the WS2812-rpi library
 const int TOGGLE = 21;
 const int RESET = 20;
 
+// ********************************************************************************************
+// ********************************************************************************************
+// ********************************************************************************************
+// ********************************************************************************************
+
+// PROTOTYPES
+// ==========
+
+void initializeStandardGPIO();
+
+void blue(NeoPixel*);
+void green(NeoPixel*);
+void teal(NeoPixel*);
+void red(NeoPixel*);
+void magenta(NeoPixel*);
+void amber(NeoPixel*);
+void white(NeoPixel*);
+void ice(NeoPixel*);
+void fire(NeoPixel*);
+void flagAmerican(NeoPixel*);
+void wild(NeoPixel*);
+
+void cellConnect();
+
+// ********************************************************************************************
+// ********************************************************************************************
+// ********************************************************************************************
+// ********************************************************************************************
+
+// MAIN
+// ====
+
 int main ()
 {
-    initializeStandardGPIO()
+    initializeStandardGPIO();
+    NeoPixel * n = new NeoPixel(25); // TODO: replace with proper LED count
+
+    int selection = 0;
+    while (true)
+    {
+        if (digitalRead(TOGGLE) == 1)
+        {
+            ++selection;
+            if (selection >= 11)
+                selection = 0;
+        }
+        switch (selection)
+        {
+            case 0:
+                blue(n);
+                break;
+            case 1:
+                green(n);
+                break;
+            case 2:
+                teal(n);
+                break;
+            case 3:
+                red(n);
+                break;
+            case 4:
+                magenta(n);
+                break;
+            case 5:
+                amber(n);
+                break;
+            case 6:
+                white(n);
+                break;
+            case 7:
+                ice(n);
+                break;
+            case 8:
+                fire(n);
+                break;
+            case 9:
+                flagAmerican(n);
+                break;
+            case 10:
+                wild(n);
+                break;
+            default:
+                // TODO: app partner
+                break;
+
+        }
+    }
     // TODO: do stuff
     return 0;
 }
 
+// ********************************************************************************************
+// ********************************************************************************************
+// ********************************************************************************************
+// ********************************************************************************************
+
+// FUNCTION DEFINITIONS
+// ====================
+
 // initializes the pins for the necessary uses as inputs and outputs
+// inputs: none
+// outputs: sets pins to input and output as needed
 void initializeStandardGPIO()
 {
     wiringPiSetup();
     
-    pinMode(RGB_LED, OUTPUT);
+    // inputs
+    pinMode(RESET, INPUT);
+    pinMode(TOGGLE, INPUT);
+
+    // outputs
+    pinMode(RGB_LED[0], OUTPUT);
+    pinMode(RGB_LED[1], OUTPUT);
+    pinMode(RGB_LED[2], OUTPUT);
+
+    pinMode(RGB_EXT[0], OUTPUT);
+    pinMode(RGB_EXT[1], OUTPUT);
+    pinMode(RGB_EXT[2], OUTPUT);
+}
+
+//
+// inputs: 
+// outputs: 
+void blue(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void green(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void teal(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void red(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void magenta(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void amber(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void white(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void ice(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void fire(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void flagAmerican(NeoPixel * n)
+{
+    // TODO: define function
+}
+
+//
+// inputs: 
+// outputs: 
+void wild(NeoPixel * n)
+{
+    // TODO: define function
 }
