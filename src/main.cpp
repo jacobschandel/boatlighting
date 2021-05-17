b55a1834949c91c1118e4f10bebdaa5ae08a5b0b
Jacob H. Schandel <jhs51@uakron.edu>
2021-05-17 17:26:59 -0400

    Add beginning of C++ master program

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @author Jacob H. Schandel
+ * @brief main executable for lighting program
+ * @version 2.0
+ * @date 2021-05-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "~       BOAT LIGHTING CONTROL SYSTEM       ~" << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+
+    return 0;
+}
